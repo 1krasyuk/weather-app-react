2ad523f51a4d023a30778a03eeb293491b7d4f9e
1krasyuk <evgeniyk687@gmail.com>
2025-12-12 23:31:44 +0300

    feat: do hourly forecast by days with select menu

--- components/HourlyForecastList.jsx
+++ components/HourlyForecastList.jsx
@@ -1,16 +1,59 @@
 import { DropdownIcon } from "./icons/DropdownIcon";
 import HourlyForecastCard from "./HourlyForecastCard";
+import { useState, useEffect } from "react";
 
 export default function HourlyForecastList({ hourlyData }) {
-  if (!hourlyData) return;
+  function groupHourlyByDay(hourly) {
+    if (!hourly || !hourly.time) return {};
+
+    const grouped = {};
+
+    hourly.time.forEach((ts, i) => {
+      const day = ts.split("T")[0]; // "2025-11-01"
+      if (!grouped[day]) grouped[day] = [];
+
+      grouped[day].push({
+        time: ts,
+        temp: hourly.temperature_2m?.[i],
+        icon: hourly.weather_code?.[i],
+      });
+    });
+
+    return grouped;
+  }
+
+  const grouped = groupHourlyByDay(hourlyData);
+  const days = Object.keys(grouped);
+
+  const [selectedDay, setSelectedDay] = useState(days[0] || null);
+
+  useEffect(() => {
+    if (!selectedDay && days.length > 0) {
+      setSelectedDay(days[0]);
+    }
+  }, [days, selectedDay]);
+
   return (
     <div className="bg-neutral-800 mt-8 px-4 py-5  rounded-2xl ">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-medium tracking-wide">Hourly forecast</h2>
-        <button className="bg-neutral-600 text-md  px-4 py-1.5 rounded-lg flex items-center gap-3">
+        <div className="relative w-fit">
+          <select
+            className="w-34 bg-neutral-600 rounded-lg text-md px-3 py-1 appearance-none cursor-pointer"
+            value={selectedDay}
+            onChange={(e) => setSelectedDay(e.target.value)}
+          >
+            {Object.keys(grouped).map((day) => (
+              <option key={day} value={day}>
+                {new Date(day).toLocaleDateString("en-US", { weekday: "long" })}
+              </option>
+            ))}
+          </select>
+          <DropdownIcon className="absolute right-3 top-1/2 -translate-y-1/2 w-3 h-3 pointer-events-none" />
+        </div>
+        {/* <button className="bg-neutral-600   px-4 py-1.5 rounded-lg flex items-center gap-3">
           Tuesday
-          <DropdownIcon className="w-3 h-3 transition-transform duration-150 " />
-        </button>
+        </button> */}
       </div>
       <div
         className="flex flex-col gap-4 max-h-145  overflow-y-auto "
@@ -21,12 +64,12 @@ export default function HourlyForecastList({ hourlyData }) {
           marginRight: "-16px",
         }}
       >
-        {hourlyData.time.map((hour, i) => (
+        {grouped[selectedDay]?.map((item) => (
           <HourlyForecastCard
-            key={hour}
-            icon={hourlyData.weather_code[i]}
-            hour={hourlyData.time[i]}
-            temp={hourlyData.temperature_2m[i]}
+            key={item.time}
+            hour={item.time}
+            temp={item.temp}
+            icon={item.icon}
           />
         ))}
       </div>
