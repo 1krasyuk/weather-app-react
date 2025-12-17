81e7d20c9795c5455488d253da51f6efaac927fe
1krasyuk <evgeniyk687@gmail.com>
2025-12-17 22:46:49 +0300

    feat: make SearchBar for desktop in one line

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -14,26 +14,44 @@ export default function SearchBar() {
   };
 
   return (
-    <form className="space-y-3" onSubmit={handleSubmit}>
-      {/* desktop:flex desktop:w-200 desktop:gap-4 desktop:items-center desktop:mx-auto */}
-      <div className="relative ">
-        <SearchIcon className="absolute left-6 top-1/2 transform -translate-y-1/2 w-5.5 h-5.5 text-neutral-200" />
+    <form
+      onSubmit={handleSubmit}
+      className="
+      space-y-3
+    desktop:space-y-0
+    desktop:flex desktop:items-center desktop:gap-4
+    desktop:max-w-2xl desktop:mx-auto
+  "
+    >
+      <div className="relative flex-1">
+        <SearchIcon className="absolute left-6 top-1/2 -translate-y-1/2 w-5.5 h-5.5 text-neutral-200" />
         <input
-          className="
-      w-full pl-15 py-3 
-      bg-neutral-800 text-neutral-0 font-normal text-xl
-      rounded-xl 
-      border-2 border-transparent
-      focus:border-neutral-0 focus:outline-none
-      placeholder:text-neutral-200 "
-          placeholder="Search for a place..."
           ref={inputRef}
-          defaultValue={searchParams.get("city")}
+          defaultValue={searchParams.get("city") ?? ""}
+          placeholder="Search for a place..."
+          className="
+        w-full pl-15 py-3
+        bg-neutral-800 text-neutral-0 text-xl
+        rounded-xl
+        border-2 border-transparent
+        focus:border-neutral-0 focus:outline-none
+        placeholder:text-neutral-200
+      "
         />
       </div>
+
       <button
         type="submit"
-        className="bg-blue-500 w-full rounded-xl py-3.5 text-xl cursor-pointer"
+        className="
+        w-full
+    desktop:w-auto
+      bg-blue-500
+      px-8
+      py-3.5
+      text-xl
+      rounded-xl
+      whitespace-nowrap
+    "
       >
         Search
       </button>
